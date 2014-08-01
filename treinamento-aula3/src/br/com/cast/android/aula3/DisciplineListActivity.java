d0e785c53f665033e1e65d2c5387dff180e1e457
veniltonjr <venilton.falvo@gmail.com>
2014-08-01 15:54:58 -0300

    Commit Inicial "treinamento-aula3"

---
+++ treinamento-aula3/src/br/com/cast/android/aula3/DisciplineListActivity.java
@@ -0,0 +1,12 @@
+package br.com.cast.android.aula3;
+
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+
+import android.support.v7.app.ActionBarActivity;
+
+@EActivity(R.layout.activity_discipline_list)
+@OptionsMenu(R.menu.discipline_list)
+public class DisciplineListActivity extends ActionBarActivity {
+
+}
